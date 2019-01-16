35aa9534682e4b42225f7b0f2edd9ef4e2b91724
Clodiap <clodiapp@gmail.com>
2019-01-16 11:59:45 +0100

    méthodes et rspec

--- lib/app.rb
+++ lib/app.rb
@@ -4,17 +4,41 @@ require 'twitter' # appelle la gem twitter
 Dotenv.load # Ceci appelle le fichier .env grâce à la gem Dotenv, et importe toutes les données enregistrées dans un hash ENV
 
 # quelques lignes qui appellent les clés d'API de ton fichier .env
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+def login_twitter
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  return client
 end
 
+def send_tweet(array, message)
+    array.each do |i|
+    login_twitter.update("@#{i}, #{message}")
+  end
+end
+
+send_tweet(["dany_about", "hsravo", "Arbousier5", "NguyenJulien5", "the_hacking_pro"], "Coucou la team À l'arrache :) ")
+
+# login_twitter.update("@NguyenJulien5, coucou on tweet depuis Ruby @the_hacking_pro")
+
 # ligne qui permet de tweeter sur ton compte
-# client.update('Mon premier tweet en Ruby !!!!')
-client.follow("gem")
-user.location("Arbousier")
+# login_twitter.update('Mon 2ème tweet en Ruby !!!! @the_hacking_pro')
+
+# the_team = ["dany_about", "hsravo", "Arbousier5", "NguyenJulien5"]
+
+# the_team.each do |i|
+#   login_twitter.follow(i)
+# end
+
+
+# ligne qui permet de follower un compte
+# login_twitter.follow("Arbousier5")
+
+#
+# login_twitter.friends("Arbousier")
 
 
 
